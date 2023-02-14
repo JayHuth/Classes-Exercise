355f7735a0d220aa61e2346f17b0d291b09d756c
Justin Huth <Jayhuth96@gmail.com>
2023-02-14 09:57:19 -0600

    Completed the excercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Tacoma";
+            myCar.Year = 2024;
+
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Model = "Raptor",
+                Year = 2023
+            };
+
+            var mercedes = new Car("Mercedes", "Sprinter", 2019);
+
+            var carList = new List<Car>() { myCar, mercedes, ford };
+
+            foreach(var vehicle in carList) 
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
         }
+
     }
 }
